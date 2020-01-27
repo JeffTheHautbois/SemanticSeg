c9440fb57542e03d90a0caffdd3805d583e39288
Jeff Peng <jeffpengyj@hotmail.com>
2020-01-26 21:03:50 -0500

    refined frame/mask processing with similarity score

--- BackgroundSub.py
+++ BackgroundSub.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import time
 import imutils
-from skimage.measure import compare_ssim
+from skimage.metrics import structural_similarity
 
 def initialize_bg_fg(capture, back_sub):
     width = capture.get(3)
@@ -21,8 +21,8 @@ def initialize_bg_fg(capture, back_sub):
         if frame is None:
             print("Error no frame")
             break
-    fg_mask = back_sub.apply(frame)
-    og_frame = frame
+        fg_mask = back_sub.apply(frame)
+        og_frame = frame
     # End time
     end = time.time()
 
@@ -52,20 +52,20 @@ def extract_diff(image_initial, image_final):
     gray_initial = cv.cvtColor(image_initial, cv.COLOR_BGR2GRAY)
     gray_final = cv.cvtColor(image_final, cv.COLOR_BGR2GRAY)
 
-    (score, diff) = compare_ssim(gray_initial, gray_final, full=True)
+    (score, diff) = structural_similarity(gray_initial, gray_final, full=True)
     diff = (diff * 255).astype("uint8")
     thresh = cv.threshold(diff, 0, 255, cv.THRESH_BINARY_INV | cv.THRESH_OTSU)[1]
-    cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-
-
-    for c in cnts:
-        # compute the bounding box of the contour and then draw the
-        # bounding box on both input images to represent where the two
-        # images differ
-        (x, y, w, h) = cv.boundingRect(c)
-        cv.rectangle(image_initial, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        cv.rectangle(image_final, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    # cnts = cv.findContours(thresh.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    # cnts = imutils.grab_contours(cnts)
+    #
+    #
+    # for c in cnts:
+    #     # compute the bounding box of the contour and then draw the
+    #     # bounding box on both input images to represent where the two
+    #     # images differ
+    #     (x, y, w, h) = cv.boundingRect(c)
+    #     cv.rectangle(image_initial, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    #     cv.rectangle(image_final, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     # show the output images
     cv.imshow("Original", image_initial)
@@ -83,19 +83,38 @@ def main():
 
     width, height, fps, og_bg_mask, og_frame = initialize_bg_fg(capture, back_sub)
     cv.imshow('og_mask', og_bg_mask)
+    cv.imshow('og_frame', og_frame)
     print(width, height, fps)
     k = cv.waitKey(0)
     counter = 0
     if k == 0x1b:
+        cv.destroyAllWindows()
         while True:
-            for i in range(0, int(fps)):
+            #get current mask
+            for i in range(0, int(fps/2)):
                 ret, frame = capture.read()
             cur_mask = back_sub.apply(frame)
             mask_diff = mse(og_bg_mask, cur_mask)
-            if (mask_diff > 150):
+            #if mask_diff greater than 2000 starts updating background sub.
+
+            #if mask_diff becomes less thant 200 set new og_background.
+
+            while (mask_diff > 200):
+                ret, frame = capture.read()
+                cur_mask = back_sub.apply(frame)
+                mask_diff = mse(og_bg_mask, cur_mask)
                 counter += 1
-            elif(mask_diff < 150 and counter > 3):
-                print("subtract image")
+
+            if(mask_diff < 200 and counter > 30):
+                print("subtract image", counter)
+                counter = 0
+                ret, new_frame = capture.read()
+                extract_diff(og_frame, new_frame)
+                # cv.imshow("new_frame", new_frame)
+                # gray_initial = cv.cvtColor(new_frame, cv.COLOR_BGR2GRAY)
+                # cv.imshow("gray_frame", gray_initial)
+                cv.waitKey(0)
+                break
             print(mask_diff)
 
 
